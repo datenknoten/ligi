502d7ff6ef730c6629b9bda07cf954d94ea726a3
Tim Schumacher <tim@datenknoten.me>
2015-08-15 14:39:14 +0200

    Figure out i18n.

---
+++ src/Datenknoten/LigiBundle/EventListener/RequestListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Datenknoten\LigiBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class RequestListener
+{
+    public function setLocale(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+        $request = $event->getRequest();
+        $browser_locale = $request->getPreferredLanguage();
+
+        $session_locale = $request->getSession()->get('_locale',null);
+        if ($session_locale) {
+            $request->setLocale($session_locale);
+        } else {
+            $request->getSession()->set('_locale', $browser_locale);
+            $request->setLocale($browser_locale);
+        }
+    }
+}
