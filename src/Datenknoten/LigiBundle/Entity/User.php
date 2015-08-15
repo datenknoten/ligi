6251b587dc4b9b6cc463e12e1b715a9218565104
Tim Schumacher <tim@datenknoten.me>
2015-08-15 18:11:10 +0200

    Integrated user authentication.

---
+++ src/Datenknoten/LigiBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Datenknoten\LigiBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+}
