4867801605191e05c18ad6c8a0c88cf9fda85aa6
Tim Schumacher <tim@datenknoten.me>
2015-08-16 23:53:18 +0200

    Implemented a simple search.

---
+++ src/Datenknoten/LigiBundle/Controller/SearchController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Datenknoten\LigiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ *
+ * search Controller
+ *
+ * @Route("/search")
+ */
+class SearchController extends Controller
+{
+    /**
+     * @Route("/{search_type}/{search}",name="ligi_search", defaults={"search"=""})
+     * @Template()
+     */
+    public function indexAction($search_type,$search, Request $request)
+    {
+        $_search = $request->request->get('search',null);
+        if (!is_null($_search)) {
+            return $this->redirect($this->generateUrl('ligi_search', ['search_type' => $search_type,'search' => $_search]));
+        }
+        $entities = [];
+
+        $search_term = ($search == '' ? '%' : $search);
+        if (strpos($search_term,'%') === false) {
+            $search_term = '%'.$search_term.'%';
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select(array('i'))
+            ->from('LigiBundle:Item', 'i')
+            ->where('i.name LIKE :name')
+            ->andWhere('i.description LIKE :description')
+            ->andWhere('i.is_request = :is_request')
+            ->orderBy('i.id')
+            ->setParameter('name', $search_term)
+            ->setParameter('description',$search_term)
+            ->setParameter('is_request',($search_type == 'request' ? true : false));
+        $entities = $qb->getQuery()->execute();        
+        
+        return [
+            "search_type" => $search_type,
+            "search" => $search,
+            "entities" => $entities,
+        ];
+    }
+
+}
