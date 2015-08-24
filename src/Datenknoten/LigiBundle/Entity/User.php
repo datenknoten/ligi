901cfe171059daa066ece88c7674a40d15203576
Tim Schumacher <tim@datenknoten.me>
2015-08-24 20:18:56 +0200

    Make the languages optional.

--- src/Datenknoten/LigiBundle/Entity/User.php
+++ src/Datenknoten/LigiBundle/Entity/User.php
@@ -27,7 +27,7 @@ class User extends BaseUser implements ParticipantInterface, UserInterface
     /**
      * @var array
      *
-     * @ORM\Column(name="languages", type="json_array",options={"default"=""})
+     * @ORM\Column(name="languages", type="json_array",options={"default"=""},nullable=true)
      */
     public $languages;
 
